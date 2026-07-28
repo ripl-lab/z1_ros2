3b0fffcda8b241f4fee6af13d2a92e66d9a31d5c
heywoode <heywoode@mcmaster.ca>
2026-07-28 10:29:00 -0400

    New waypoints

---
+++ z1_examples/z1_examples/waypoint_past.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 IDRA, University of Trento
+# Author: Matteo Dalle Vedove (matteodv99tn@gmail.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+import numpy as np
+from rclpy.action import ActionClient
+from rclpy.node import Node
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from control_msgs.action import FollowJointTrajectory
+
+
+
+class TrajectoryCaller(Node):
+
+    def __init__(self):
+        super().__init__("joint_trajectory_caller")
+
+        self.get_logger().info("Creating client")
+        self.action_client = ActionClient(
+            self,
+            FollowJointTrajectory,
+            "/joint_trajectory_controller/follow_joint_trajectory",
+        )
+        self.action_client.wait_for_server()
+        self.get_logger().info("Connection established")
+
+    def send_traj_request(self, positions: list[np.ndarray]):
+        self.get_logger().info("Creating trajectory request")
+        traj_msg = FollowJointTrajectory.Goal()
+        traj: JointTrajectory = traj_msg.trajectory
+        traj.joint_names = [
+            "joint1",
+            "joint2",
+            "joint3",
+            "joint4",
+            "joint5",
+            "joint6",
+        ]
+        for i, pos in enumerate(positions):
+            self.get_logger().info(f"{pos}")
+            pt = JointTrajectoryPoint()
+            pt.positions = pos.tolist()
+            pt.time_from_start.sec = (i+1) * 2
+            traj.points.append(pt)
+
+        self.get_logger().info("Sending trajectory request")
+        return self.action_client.send_goal_async(traj_msg)
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = TrajectoryCaller()
+
+    future = node.send_traj_request([
+        np.array([0.0, 0.5, -0.4, 0.0, 0.0, 0.0]),
+        np.array([0.0, 0.5, -0.5, 0.0, 0.0, 0.0]),
+        np.array([0.0, 0.5, -0.6, 0.0, 0.0, 0.0]),
+        np.array([0.0, 0.5, -0.7, 0.0, 0.0, 0.0]),
+
+        np.array([-0.1, 0.8, -0.7, 0.0, 0.0, 0.0]),
+        np.array([-0.2, 1.0, -1.0, 0.0, 0.4, 0.0]),
+        np.array([-0.3, 1.2, -1.1, 0.0, 0.5, 0.1]),
+        np.array([-0.4, 1.3, -1.2, 0.0, 0.6, 0.12]),
+        np.array([-0.5, 1.4, -1.3, 0.0, 0.7, 0.15]),
+        np.array([-0.5, 1.5, -1.5, 0.0, 0.8, 0.12]),
+        np.array([-0.6, 1.6, -1.6, 0.0, 0.9, 0.2]),
+        np.array([-0.6, 1.6, -1.7, 0.0, 1.0, 0.2]),
+        np.array([-0.7, 1.7, -1.8, 0.0, 1.1, 0.2]),
+        np.array([-0.8, 1.7, -1.9, 0.0, 1.1, 0.2]),
+        np.array([-0.8, 1.8, -2.1, 0.0, 1.34, 0.25]),
+
+        np.array([-0.9, 2.0, -2.4, 0.0, 1.34, 0.25]),
+        np.array([-0.9, 2.0, -2.3, 0.0, 1.34, 0.15]),
+        np.array([-0.9, 2.0, -2.2, 0.0, 1.34, 0.1]),
+        np.array([-0.9, 2.0, -2.0, 0.0, 1.34, -0.07]),
+        np.array([-0.9, 2.0, -2.0, 0.0, 1.34, -0.13]),
+        np.array([-0.9, 2.0, -1.9, 0.0, 1.34, -0.2]),
+        np.array([-0.9, 2.0, -1.8, 0.0, 1.34, -0.25]),
+        np.array([-0.9, 2.0, -1.8, 0.0, 1.34, -0.3]),
+        np.array([-0.9, 2.0, -1.7, 0.0, 1.34, -0.4]),
+        np.array([-0.9, 2.0, -1.6, 0.0, 1.34, -0.5]),
+        np.array([-0.9, 2.0, -1.5, 0.0, 1.34, -0.6]),
+        np.array([-0.9, 2.0, -1.4, 0.0, 1.34, -0.7]),
+        np.array([-0.9, 2.0, -1.3, 0.0, 1.34, -0.8]),
+        np.array([-1.0, 2.0, -1.2, 0.0, 1.34, -0.9]),
+        np.array([-1.0, 2.0, -1.1, 0.0, 1.34, -1.0]),
+        np.array([-1.1, 2.0, -1.0, 0.0, 1.34, -1.1]),
+
+        # np.array([-1.1, 2.0, -1.1, 0.0, 1.34, -1.2]),
+        # np.array([-1.2, 2.0, -1.0, 0.0, 1.34, -1.2]),
+        # np.array([-1.1, 2.0, -0.9, 0.0, 1.34, -1.3]),
+        # np.array([-1.2, 2.0, -0.8, 0.0, 1.34, -1.3]),
+
+        # np.array([-1.2, 2.0, -0.9, 0.0, 1.34, -1.2]),
+        # np.array([-1.2, 2.0, -1.0, 0.0, 1.34, -1.1]),
+        # np.array([-1.2, 2.0, -1.1, 0.0, 1.34, -0.9]),
+        # np.array([-1.2, 2.0, -1.2, 0.0, 1.34, -0.9]),
+
+        # np.array([-1.2, 2.0, -1.0, 0.0, 1.3, -0.9]),
+        # np.array([-1.2, 2.0, -0.9, 0.0, 1.25, -1.0]),
+        # np.array([-1.2, 2.0, -0.8, 0.0, 1.2, -1.1]),
+        # np.array([-1.2, 2.0, -0.7, 0.0, 1.0, -1.2]),
+        # np.array([-1.2, 2.0, -0.6, 0.0, 1.0, -1.3]),
+
+        # np.array([-1.1, 2.0, -0.6, 0.0, 0.9, -1.3]),
+        # np.array([-0.9, 2.0, -0.7, 0.0, 0.9, -1.2]),
+        # np.array([-0.8, 2.0, -0.7, 0.0, 0.8, -1.0]),
+        # np.array([-0.8, 2.0, -0.8, 0.0, 0.7, -0.9]),
+        # np.array([-0.7, 2.0, -0.8, 0.0, 0.6, -0.8]),
+        # np.array([-0.6, 2.0, -0.9, 0.0, 0.5, -0.7]),
+        # np.array([-0.4, 2.0, -0.9, 0.0, 0.4, -0.6]),
+        # np.array([-0.3, 2.0, -1.0, 0.0, 0.3, -0.5]),
+        # np.array([-0.5, 2.0, -1.0, 0.0, 0.2, -0.4]),
+        # np.array([-0.4, 2.0, -1.0, 0.0, 0.1, -0.3]),
+        # np.array([-0.3, 2.0, -1.1, 0.0, 0.1, -0.2]),
+        # np.array([-0.2, 2.0, -1.2, 0.0, 0.0, -0.1]),
+        # np.array([-0.1, 2.0, -1.2, 0.0, 0.0, 0.0]),
+
+        # np.array([0.0, 2.0, -1.3, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.95, -1.3, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.9, -1.4, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.9, -1.4, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.9, -1.4, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.9, -1.4, 0.0, -0.3, 0.0]),
+        # np.array([0.0, 1.7, -1.4, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.7, -1.4, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.7, -1.4, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.6, -1.4, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.6, -1.4, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.6, -1.4, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.5, -1.4, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.5, -1.4, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.5, -1.4, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.5, -1.4, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.5, -1.5, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.5, -1.5, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.5, -1.5, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.4, -1.5, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.4, -1.5, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.4, -1.5, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.4, -1.5, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.3, -1.5, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.3, -1.5, 0.0, 0.5, 0.0]),
+        # np.array([0.0, 1.3, -1.5, 0.0, 0.0, 0.0]),
+        # np.array([0.0, 1.3, -1.5, 0.0, -0.5, 0.0]),
+        # np.array([0.0, 1.3, -1.5, 0.0, 0.0, 0.0]),
+
+        np.array([0.0, 1.2, -1.5, 0.0, 0.5, 0.0]),
+        np.array([0.0, 1.2, -1.5, 0.0, 0.0, 0.0]),
+        np.array([0.0, 1.2, -1.5, 0.0, -0.5, 0.0]),
+        np.array([0.0, 1.2, -1.5, 0.0, -0.6, 0.0]),
+        np.array([0.1, 1.2, -1.5, 0.0, -0.7, 0.0]),
+        np.array([0.2, 1.2, -1.6, 0.0, -0.8, 0.0]),
+        np.array([0.3, 1.3, -1.7, 0.0, -0.9, 0.0]),
+        np.array([0.4, 1.4, -1.8, 0.0, -0.9, 0.0]),
+        np.array([0.5, 1.5, -1.9, 0.0, -1.0, 0.0]),
+        np.array([0.6, 1.6, -2.0, 0.0, -1.1, 0.0]),
+        np.array([0.7, 1.7, -2.1, 0.0, -1.2, 0.0]),
+        np.array([0.8, 1.8, -2.2, 0.0, -1.3, 0.0]),
+
+        np.array([0.9, 2.0, -2.4, 0.0, -1.34, -0.25]),
+        np.array([0.9, 2.0, -2.2, 0.0, -1.34, -0.1]),
+        np.array([0.9, 2.0, -2.0, 0.0, -1.34, 0.07]),
+        np.array([0.9, 2.0, -1.8, 0.0, -1.34, 0.3]),
+        np.array([0.9, 2.0, -1.6, 0.0, -1.34, 0.5]),
+        np.array([0.9, 2.0, -1.4, 0.0, -1.34, 0.7]),
+        np.array([1.0, 2.0, -1.2, 0.0, -1.34, 0.9]),
+        np.array([1.1, 2.0, -1.0, 0.0, -1.34, 1.1]),
+        np.array([1.2, 2.0, -0.8, 0.0, -1.34, 1.3]),
+        
+       
+    
+        
+        
+    ])
+    rclpy.spin_until_future_complete(node, future)
+
+
+
+if __name__ == '__main__':
+    main()
